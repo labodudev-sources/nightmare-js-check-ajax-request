90d9793aa56249b6c72c850e8b8cdf40e6f052ae
Jimmy <jimmy@eoxia.com>
2016-11-16 22:23:13 +0100

    Add comment

--- test.js
+++ test.js
@@ -1,27 +1,27 @@
-var Nightmare = require('nightmare');
+var Nightmare = require('nightmare'); // Chargement de NightmareJS
 var path = require('path');
 
 var nightmare = Nightmare({
   show: true,
   webPreferences: {
-    preload: path.resolve("xhr.js")
+    preload: path.resolve("xhr.js") // Ajout du script personnalisé pour récupérer les réponses XHR
   }
 });
 
 nightmare
-  .goto('http://127.0.0.1/nightmare-js-check-ajax-request/')
+  .goto('http://127.0.0.1/nightmare-js-check-ajax-request/') // Charge la page
   .type('input[type="text"]', "This input is not a number :)")
-  .click('input[type="submit"]')
+  .click('input[type="submit"]') // Valide le formulaire
   .wait(function() {
-    if (window.currentRequestResponse) {
+    if (window.currentRequestResponse) { // Tourne en boucle en attendant que window.currentRequestResponse soit définie
       return true;
     }
   })
   .evaluate(function() {
-    return window.currentRequestResponse;
+    return window.currentRequestResponse; // Retourne la variable pour l'utiliser dans la fonction then
   })
   .then(function(response) {
-    if (isNaN(response)) {
+    if (isNaN(response)) { // Vérifie si ce n'est pas un nombre
 		console.error('It\'s not a number!');
 	}
 	else {
